301d1585549eac2ee371e2bd41b01563d20d2ce6
Justin Ruiter | s2264609 <j.a.g.ruiter@student.utwente.nl>
2021-03-11 22:47:45 +0100

    Content type added, make use of htaccess

--- private/updates.php
+++ private/updates.php
@@ -2,9 +2,9 @@
     if (file_exists("/var/secret/api.elitelooter.com/secret/priv_keys.php")) {
         include_once '/var/secret/api.elitelooter.com/secret/priv_keys.php';
     } else {
-        include_once dirname(__FILE__).'/../../secret/priv_keys.php';
+        include_once dirname(__FILE__) . '/../secret/priv_keys.php';
     }
-    require(dirname(__FILE__).'/../../util.php');
+    require(dirname(__FILE__) . '/../util.php');
 
     // Check if the request is authorized
     if (getAuthHeader(apache_request_headers()) != $updates_key) { errorAndExit(null, "HTTP/1.1 401 UNAUTHORIZED"); }
@@ -12,6 +12,7 @@
     $requestMethod = $_SERVER["REQUEST_METHOD"];
     if ($requestMethod == "GET") {
         if (isset($_GET["data"]) && $_GET["data"] == "nextupdate") {
+            header("Content-Type:application/json");
             echo json_encode(array("update_known" => false,
                             "update_version" => "4.0-alpha-1",
                             "short_description" => "The first alpha release of elite looter. Currently doesn't have a release date yet.",
